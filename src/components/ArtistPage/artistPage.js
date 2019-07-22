11bd461fefa6ed2a19f0577adcbb5936bfd5e9e1
Ramesh <rameshnaidu.allu@gmail.com>
2019-07-22 12:42:49 +0530

    fix(route):routing issue resolved

--- src/components/ArtistPage/artistPage.js
+++ src/components/ArtistPage/artistPage.js
@@ -1,10 +1,10 @@
 import React from 'react';
-
-
+import {Link} from 'react-router-dom';
 
 const ArtistPage = (props) => {
     return (
         <div>
+        <Link to="/">Back to Home</Link>
         <h1>Artist Page</h1>
         <h1>{props.match.params.id}</h1>
         </div>
